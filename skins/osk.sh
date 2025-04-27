4aea568e09523d136c42dcd4d9d2c7a35bdf173c
eufi <eufino@proton.me>
2025-04-27 19:09:59 +0200

    Add script
    Bash script that creates an osk zip file using 7zip.

---
+++ skins/osk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+name=$1
+echo "Creating osk for: "$name
+if [ -f "$name.osk" ]; then
+  rm "$name.osk"
+  echo "Removed old osk"
+fi
+7z a "$name".zip -y -bsp0 -bso0 ./"$name" && mv "$name".zip "$name".osk && ls -lh "$name".osk
